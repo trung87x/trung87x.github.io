00b66dac4cc0b7587b38e06876411243fc5bd758
trung87x <job.dinhquangtrung@gmail.com>
2026-01-24 17:30:02 +0700

    .

--- src/app/(features)/bricksbuilder/page.jsx
+++ src/app/(features)/bricksbuilder/page.jsx
@@ -1,12 +1,4 @@
 export default function Example() {
-  return (
-    <>
-      <section className="flex flex-col items-center justify-center self-stretch">
-        <div className="flex flex-col items-center justify-center self-stretch">
-          abc
-        </div>
-      </section>
-    </>
-  );
+  return <></>;
 }
 //
